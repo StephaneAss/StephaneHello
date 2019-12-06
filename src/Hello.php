0f045059f9da144f4a2de6f0e7950be641e5f1a6
Stephane <mensste30@gmail.com>
2019-12-06 11:01:56 +0100

    ajout de licence

--- src/Hello.php
+++ src/Hello.php
@@ -1,6 +1,9 @@
 <?php 
-/**
- * 
+/*
+ * (c) YOUR NAME <your@email.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 namespace StephaneAss\StephaneHello;
 
